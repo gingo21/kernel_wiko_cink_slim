fc10594829a860df8bfa27c0dcf052776eaf6def
gingo21 <francois.reymond10@gmail.com>
2013-03-08 00:47:05 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+make -j84 TARGET_PRODUCT=s8073 MTK_ROOT_CUSTOM=../mediatek/custom CROSS_COMPILE=/home/fr/Documents/slim/prebuilt/linux-x86/toolchain/arm-linux-androideabi-4.4.x/bin/arm-linux-androideabi-
+
